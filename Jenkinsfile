42b1baa25bc997e378c51b1fe237334064657133
kishore <kishore.9686@gmail.com>
2025-04-09 09:44:02 +0530

    add required data

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,7 +1,8 @@
-// create a jenkinsfile with all stages
 // Calling the shared library
 
 @Library("com.i27academy.slb@main") _
-nodePipeline(
-    appName: 'clothing'
+dockerPipeline (
+    appName: 'clothing',
+    devHostPort: '9001',
+    contPort: '3000'
 )
--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const produtsServiceLocalHost = 'http://localhost:8132/api'
 const usersServiceLocalHost = 'http://localhost:8232/api'
 
-const produtsServiceDevHost = 'http://104.198.52.21:30263/api' 
-const usersServiceDevHost = 'http://104.198.52.21:31904/api'
+const produtsServiceDevHost = 'http://34.21.85.198:6001/api' 
+const usersServiceDevHost = 'http://34.21.85.198:8001/api'
 
 const produtsServiceTestHost = ''
 const usersServiceTestHost = ''
